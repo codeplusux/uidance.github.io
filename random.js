a3b2f62e4934b2d1819b7082ea8a9cf7481e58cb
codeplusux <dev@pablovivanco.com>
2020-07-29 16:11:26 -0400

    new random function

--- random.js
+++ random.js
@@ -1,6 +1,22 @@
-var videosArray = ["videos/rappi.mp4", "videos/trivago.mp4"];
+// var videosArray = ["videos/rappi.mp4", "videos/trivago.mp4"];
+
+var videosArray = [
+  {
+    id: "id1",
+    name: "Rappi",
+    path: "videos/rappi.mp4",
+  },
+  {
+    id: "id2",
+    name: "Trivago",
+    path: "videos/trivago.mp4",
+  },
+];
 
 function nextDance() {
-  var rand = Math.floor(Math.random() * videosArray.length);
-  document.getElementById("stage").src = videosArray[rand];
+  const { name, path } = videosArray[
+    Math.floor(Math.random() * videosArray.length)
+  ];
+  document.getElementById("hero").innerHTML = name;
+  document.getElementById("stage").src = path;
 }
