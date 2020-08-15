57619080a930311143651630afa10c44a468b64e
codeplusux <dev@pablovivanco.com>
2020-08-14 23:40:40 -0400

    new layout :)

--- dance.js
+++ dance.js
@@ -1,43 +1,35 @@
-// Loading
-let cargando = document.querySelector(".loader");
-
-// window.addEventListener("load", function () {
-//   cargando.style.display = "none";
-// });
-
-// Video player
-var video = document.querySelector(".stage");
+var video = document.querySelector(".srcVideo");
 var progressBar = document.querySelector(".progressBar");
 var btn = document.getElementById("play-pause");
+let currentTime = 0;
 
 function tooglePlayPause() {
   if (video.paused) {
+    currentTime = video.currentTime;
+    video.load();
     btn.className = "pause";
     video.play();
     document.getElementsByTagName("body")[0].setAttribute("class", "videoOn");
-    document.getElementsByTagName("div")[0].classList.add("noBg");
-    document.getElementById("tagline").classList.add("taglineOn");
+    document.getElementById("mainFlex").classList.add("noBg");
+    document.getElementById("slogan").classList.add("sloganOn");
     document.getElementById("svg1").classList.add("hide");
-    document.getElementById("svg2").classList.add("btnOn");
     document.getElementById("svg3").classList.remove("hide");
     document.getElementById("svg3").classList.add("btnOn");
     document.getElementById("bar").classList.remove("barOff");
-    document.getElementById("main").classList.add("noBg");
-    //document.getElementById("next").classList.add("invisible");
+    document.getElementById("gallery").classList.add("gridOff");
   } else {
+    video.currentTime = currentTime;
     btn.className = "play";
     video.pause();
     document
       .getElementsByTagName("body")[0]
       .removeAttribute("class", "videoOn");
     document.getElementsByTagName("div")[0].classList.remove("noBg");
-    document.getElementById("tagline").classList.remove("taglineOn");
+    document.getElementById("slogan").classList.remove("sloganOn");
     document.getElementById("svg1").classList.remove("hide");
-    document.getElementById("svg2").classList.remove("btnOn");
     document.getElementById("svg3").classList.add("hide");
     document.getElementById("bar").classList.add("barOff");
-    document.getElementById("main").classList.remove("noBg");
-    //document.getElementById("next").classList.remove("invisible");
+    document.getElementById("gallery").classList.remove("gridOff");
   }
 }
 
@@ -45,14 +37,10 @@ btn.onclick = function () {
   tooglePlayPause();
 };
 
-// Video duration
-// video.addEventListener("loadedmetadata", function () {
-//   console.log(video.duration);
-// });
-
 video.addEventListener("timeupdate", function () {
   var point = video.currentTime / video.duration;
   progressBar.style.width = point * 100 + "%";
+
   if (video.ended) {
     btn.className = "play";
     document
@@ -61,25 +49,8 @@ video.addEventListener("timeupdate", function () {
     document.getElementById("svg1").classList.remove("hide");
     document.getElementById("svg3").classList.add("hide");
     document.getElementsByTagName("div")[0].classList.remove("noBg");
-    document.getElementById("tagline").classList.remove("taglineOn");
-    document.getElementById("main").classList.remove("noBg");
+    document.getElementById("slogan").classList.remove("sloganOn");
+    document.getElementById("gallery").classList.remove("gridOff");
+    document.getElementById("bar").classList.add("barOff");
   }
 });
-
-// window.onload = function () {
-//   console.log("test");
-//   if (
-//     navigator.userAgent.match(/Android/i) ||
-//     navigator.userAgent.match(/iPhone/i) ||
-//     navigator.userAgent.match(/iPod/i) ||
-//     navigator.userAgent.match(/BlackBerry/i) ||
-//     navigator.userAgent.match(/Windows Phone/i)
-//   ) {
-//     document
-//       .getElementsByClassName("mobileAlert")
-//       .element.classList.remove("hide");
-//     console.log("mobile");
-//   } else {
-//     console.log("desktop");
-//   }
-// };
